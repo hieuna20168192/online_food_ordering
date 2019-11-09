cf6bb7fa62b169d6bebf468021891e895c7f5dca
hieuna20168192 <nguyenanhhieu231298@gmail.com>
2019-11-10 02:56:18 +0700

    This is a new commit for what I originally planned to be amended

--- Online Food Ordering/app/src/main/java/com/example/onlinefoodordering/MainActivity.java
+++ Online Food Ordering/app/src/main/java/com/example/onlinefoodordering/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+<<<<<<< HEAD
+        // Hello World War I => Git Conflict !!!
+=======
         // Hello World War II => Git Conflict !!!
+>>>>>>> 07a101da988a16fdcbe6ebe940b09f8891ea878f
     }
 }
