f0f657df9cf7e9650bc93e527d9f64718faa2ed0
hieuna20168192 <nguyenanhhieu231298@gmail.com>
2019-11-10 02:58:56 +0700

    remove conflicted comments

--- Online Food Ordering/app/src/main/java/com/example/onlinefoodordering/MainActivity.java
+++ Online Food Ordering/app/src/main/java/com/example/onlinefoodordering/MainActivity.java
@@ -10,11 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-<<<<<<< HEAD
-        // Hello World War I => Git Conflict !!!
-=======
-        // Hello World War II => Git Conflict !!!
->>>>>>> 07a101da988a16fdcbe6ebe940b09f8891ea878f
     }
 }
