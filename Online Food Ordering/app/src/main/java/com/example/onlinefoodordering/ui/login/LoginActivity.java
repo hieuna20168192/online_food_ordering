bacb1c980dcd68b2159e6c559c8b67bda96df0a5
hieuna20168192 <nguyenanhhieu231298@gmail.com>
2019-12-14 01:40:06 +0700

    add Boarding and Login screen

---
+++ Online Food Ordering/app/src/main/java/com/example/onlinefoodordering/ui/login/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.onlinefoodordering.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.onlinefoodordering.R;
+import com.example.onlinefoodordering.ui.MainActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
